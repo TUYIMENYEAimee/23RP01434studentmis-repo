7ec6630245c2cbfa1d6ea6074ed2490eb38b137e
TUYIMENYE <tuyimenyeaimee@gmail.com>
2024-03-18 15:28:48 +0200

    index page is created

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"Welcome to Students MIS"
+    ?>
+     <a href="index.php">Index Page</a> <a href="23RP01434">
+</body>
+</html>
